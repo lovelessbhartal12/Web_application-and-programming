72abb0ed6f0397fdffb32b281cb748b0ee7434b1
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-01-21 09:16:35 +0545

    js content

---
+++ javascipt/jsfolder/script.js
@@ -0,0 +1,8 @@
+console.log("hello my name is loveless bhartal");
+// import div, { add, sub as minuus } from "../jsfolder/math.js";
+import * as math from "../jsfolder/math.js";
+
+console.log(math.add(5, 7));
+console.log(math.sub(10, 5));
+
+console.log(math.div(10, 5));
