f704051fb31ea9ce7bf909acc50c8163f23c2cb5
Loblesh bhartal <lovelessbhartal12@gmail.com>
2026-02-16 12:04:38 +0545

    ADD github actions for testing

--- bookstore/registration/tests.py
+++ bookstore/registration/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .utils import add
+from django.urls import reverse
+
+class Addfunctiontestclass(TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+
+
+class RegistrationFormpage(TestCase):
+    def test_registration_contians_input_tezt(self):
+        reponse=self.client.get(reverse('registration:form'))
+        self.assertEqual(reponse.status_code,200)
+        self.assertEqual(reponse,' name')
+        self.assertEqual(reponse ,' email')
+        self.assertEqual(reponse , ' password ')
